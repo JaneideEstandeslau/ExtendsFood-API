b918abcab4466e40b57cdffa208822b672cc5f32
Janeide Estandeslau <janeide.estan@gmail.com>
2021-03-28 10:10:46 -0300

    Primeiro Commit

---
+++ src/main/java/com/digitalSystems/extendsfood/ExtendsfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.digitalSystems.extendsfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtendsfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtendsfoodApiApplication.class, args);
+	}
+
+}
