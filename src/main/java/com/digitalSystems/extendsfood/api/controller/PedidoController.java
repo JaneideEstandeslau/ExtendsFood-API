3b8ce27537a76cfe63572ce9a9a7786dc2d44832
Janeide Estandeslau <janeide.estan@gmail.com>
2021-12-13 20:46:01 -0300

    test

--- src/main/java/com/digitalSystems/extendsfood/api/controller/PedidoController.java
+++ src/main/java/com/digitalSystems/extendsfood/api/controller/PedidoController.java
@@ -65,7 +65,7 @@ public class PedidoController implements PedidoControllerOpenApi{
 	@GetMapping
     public PagedModel<PedidoResumoModel> pesuisar(PedidoFilter filtro, @PageableDefault(size = 10) Pageable pageable) {
 		
-		System.out.println("Teste teste");
+		System.out.println("Teste teste teste");
 		Pageable pageableTraduzido = traduzirPageable(pageable);
 		
         Page<Pedido> pedidosPage = pedidoRepository.findAll(PedidoSpecs.filtrarPedidos(filtro), pageableTraduzido);
