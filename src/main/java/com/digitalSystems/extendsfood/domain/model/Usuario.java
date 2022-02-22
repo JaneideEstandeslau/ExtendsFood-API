dc7e87aed58cb49c35977c4f916243bf19d3e951
Janeide Estandeslau <janeide.estan@gmail.com>
2022-02-21 21:38:20 -0300

    test token funcionando

--- src/main/java/com/digitalSystems/extendsfood/domain/model/Usuario.java
+++ src/main/java/com/digitalSystems/extendsfood/domain/model/Usuario.java
@@ -1,9 +1,7 @@
 package com.digitalSystems.extendsfood.domain.model;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
