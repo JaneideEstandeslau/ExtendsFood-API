c6af032279eb8657fa4ce5f428f01a18cb4ea792
Janeide Estandeslau <janeide.estan@gmail.com>
2022-03-09 21:43:35 -0300

    Comentario

--- src/main/java/com/digitalSystems/extendsfood/auth/WebSecurityConfig.java
+++ src/main/java/com/digitalSystems/extendsfood/auth/WebSecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity //Habilita o WebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Override
