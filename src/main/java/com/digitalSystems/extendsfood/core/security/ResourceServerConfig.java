6d846256296db5e28b5ded4477ca96312bbcebfc
Janeide Estandeslau <janeide.estan@gmail.com>
2022-03-20 11:30:23 -0300

    Adicionado JWT com assinatura simetrica

--- src/main/java/com/digitalSystems/extendsfood/core/security/ResourceServerConfig.java
+++ src/main/java/com/digitalSystems/extendsfood/core/security/ResourceServerConfig.java
@@ -1,9 +1,14 @@
 package com.digitalSystems.extendsfood.core.security;
 
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 @EnableWebSecurity
@@ -17,7 +22,19 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter{
 		.and()
 		.cors().and()
 		.oauth2ResourceServer() //Configura o suporte ao resource server no projeto (projeto é um resource server)
-			.opaqueToken(); //Utiliza os token para dar acesso ao endpoints
+			.jwt(); //Utiliza os token JWT para dar acesso ao endpoints
+	}
+	
+	//Responsavel pela decodificação do JWT
+	@Bean
+	public JwtDecoder jetDecoder() {
+		
+		String chave = "jhgfh35gj8g3h1jkgiughfdbjhhfdgdh23189156789dfbshfghjkbsgh";
+		String metodoCriptografico = "HmacSHA256";
+		
+		var secretKey = new SecretKeySpec(chave.getBytes(), metodoCriptografico);
+		
+		return NimbusJwtDecoder.withSecretKey(secretKey).build();
 	}
 	
 	
