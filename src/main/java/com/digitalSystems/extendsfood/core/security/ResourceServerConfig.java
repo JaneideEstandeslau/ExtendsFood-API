b340c5fad71baa1482960ed8f2726fb1a809b109
Janeide Estandeslau <janeide.estan@gmail.com>
2022-03-22 20:14:14 -0300

    Adicionando JWT com assinatura assimetrica

--- src/main/java/com/digitalSystems/extendsfood/core/security/ResourceServerConfig.java
+++ src/main/java/com/digitalSystems/extendsfood/core/security/ResourceServerConfig.java
@@ -1,14 +1,9 @@
 package com.digitalSystems.extendsfood.core.security;
 
-import javax.crypto.spec.SecretKeySpec;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 @EnableWebSecurity
@@ -23,20 +18,6 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter{
 		.cors().and()
 		.oauth2ResourceServer() //Configura o suporte ao resource server no projeto (projeto é um resource server)
 			.jwt(); //Utiliza os token JWT para dar acesso ao endpoints
-	}
-	
-	//Responsavel pela decodificação do JWT
-	@Bean
-	public JwtDecoder jetDecoder() {
-		
-		String chave = "jhgfh35gj8g3h1jkgiughfdbjhhfdgdh23189156789dfbshfghjkbsgh";
-		String metodoCriptografico = "HmacSHA256";
-		
-		var secretKey = new SecretKeySpec(chave.getBytes(), metodoCriptografico);
-		
-		return NimbusJwtDecoder.withSecretKey(secretKey).build();
-	}
-	
-	
+	}	
 	
 }
