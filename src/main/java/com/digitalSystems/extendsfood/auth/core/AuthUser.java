80d8d75e9d4496b924cce4320ec9685b0c79e30c
Janeide Estandeslau <janeide.estan@gmail.com>
2022-03-27 10:18:35 -0300

    Adicionando as permissoes do usuário no payload do JWT

--- src/main/java/com/digitalSystems/extendsfood/auth/core/AuthUser.java
+++ src/main/java/com/digitalSystems/extendsfood/auth/core/AuthUser.java
@@ -1,7 +1,9 @@
 package com.digitalSystems.extendsfood.auth.core;
 
+import java.util.Collection;
 import java.util.Collections;
 
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
 import com.digitalSystems.extendsfood.auth.model.Usuario;
@@ -19,8 +21,9 @@ public class AuthUser extends User {
 	private Long userId;
 	private String fullName;
 
-	public AuthUser(Usuario usuario) {
-		super(usuario.getEmail(), usuario.getSenha(), Collections.emptyList());
+	//authorities são as permissoes dos usuários
+	public AuthUser(Usuario usuario, Collection<? extends GrantedAuthority> authorities) {
+		super(usuario.getEmail(), usuario.getSenha(), authorities);
 
 		this.userId = usuario.getId();
 		this.fullName = usuario.getNome();
--- src/main/java/com/digitalSystems/extendsfood/auth/model/Permissao.java
+++ src/main/java/com/digitalSystems/extendsfood/auth/model/Permissao.java
@@ -1,16 +1,10 @@
 package com.digitalSystems.extendsfood.auth.model;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -18,7 +12,7 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class Usuario {
+public class Permissao {
 
 	@Id
 	@EqualsAndHashCode.Include
@@ -26,20 +20,9 @@ public class Usuario {
 	private Long id;
 	
 	@Column(nullable = false)
-	private String cpf;
-
-	@Column(nullable = false)
 	private String nome;
 	
 	@Column(nullable = false)
-	private String email;
-	
-	@Column(nullable = false)
-	private String senha;
-	
-	@ManyToMany
-	@JoinTable(name = "usuario_grupo", joinColumns = @JoinColumn(name = "usuario_id"),
-			inverseJoinColumns = @JoinColumn(name = "grupo_id"))
-	private Set<Grupo> grupos = new HashSet<>();
+	private String descricao;
 	
 }
